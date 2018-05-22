f98ca3687795dbb24158ed416eab14edceae9772
Brian Tiger Chow <btc@users.noreply.github.com>
2018-05-22 18:49:09 +0000

    fix import issue

--- gatherer.go
+++ gatherer.go
@@ -49,8 +49,11 @@ func (g *gatherer) Generate(f *generator.FileDescriptor) {
 }
 
 func (g *gatherer) hydratePackage(f *generator.FileDescriptor, comments map[string]string) Package {
-	importPath := goImportPath(g.Generator.Unwrap(), f)
-	name := string(g.Generator.GoPackageName(importPath))
+	// TODO(btc): perhaps return error with specific info about failure
+	importPath, name, ok := goPackageOption(f)
+	if !ok {
+		g.Fail("could not determine import path")
+	}
 
 	g.push("package:" + name)
 	defer g.pop()
