4a50d2820cebbad06e7a00752cbfee7f7c747bc5
Chris Roche <croche@lyft.com>
2019-12-11 13:59:51 -0800

    Enable BiDi graph building (#70)
    * Enable BiDi graph building

--- workflow.go
+++ workflow.go
@@ -15,9 +15,12 @@ type workflow interface {
 }
 
 // standardWorkflow describes a typical protoc-plugin flow, with the only
-// exception being the behavior of the persistor directly writing custom file
+// exception being the behavior of the persister directly writing custom file
 // artifacts to disk (instead of via the plugin's output to protoc).
-type standardWorkflow struct{ *Generator }
+type standardWorkflow struct {
+	*Generator
+	BiDi bool
+}
 
 func (wf *standardWorkflow) Init(g *Generator) AST {
 	wf.Generator = g
@@ -36,6 +39,10 @@ func (wf *standardWorkflow) Init(g *Generator) AST {
 	wf.params = ParseParameters(req.GetParameter())
 	for _, pm := range wf.paramMutators {
 		pm(wf.params)
+	}
+
+	if wf.BiDi {
+		return ProcessCodeGeneratorRequestBidirectional(g, req)
 	}
 
 	return ProcessCodeGeneratorRequest(g, req)
