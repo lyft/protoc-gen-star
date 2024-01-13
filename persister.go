126402cbc707ea1f857c977d13d1f680a8175f42
Tricia Decker <1440268+pdecks@users.noreply.github.com>
2024-01-12 16:11:00 -0800

    persister changes on v1

--- persister.go
+++ persister.go
@@ -12,6 +12,7 @@ import (
 
 type persister interface {
 	SetDebugger(d Debugger)
+	SetOutputPath(path string)
 	SetFS(fs afero.Fs)
 	AddPostProcessor(proc ...PostProcessor)
 	Persist(a ...Artifact) *plugin_go.CodeGeneratorResponse
@@ -20,13 +21,15 @@ type persister interface {
 type stdPersister struct {
 	Debugger
 
-	fs    afero.Fs
-	procs []PostProcessor
+	outputPath string
+	fs         afero.Fs
+	procs      []PostProcessor
 }
 
 func newPersister() *stdPersister { return &stdPersister{fs: afero.NewOsFs()} }
 
 func (p *stdPersister) SetDebugger(d Debugger)                 { p.Debugger = d }
+func (p *stdPersister) SetOutputPath(path string)              { p.outputPath = path }
 func (p *stdPersister) SetFS(fs afero.Fs)                      { p.fs = fs }
 func (p *stdPersister) AddPostProcessor(proc ...PostProcessor) { p.procs = append(p.procs, proc...) }
 
@@ -69,7 +72,7 @@ func (p *stdPersister) Persist(arts ...Artifact) *plugin_go.CodeGeneratorRespons
 			p.insertFile(resp, f, false)
 		case CustomFile:
 			p.writeFile(
-				a.Name,
+				p.resolveOutputPath(a.Name),
 				[]byte(p.postProcess(a, a.Contents)),
 				a.Overwrite,
 				a.Perms,
@@ -79,7 +82,7 @@ func (p *stdPersister) Persist(arts ...Artifact) *plugin_go.CodeGeneratorRespons
 			p.CheckErr(err, "unable to render CustomTemplateFile: ", a.Name)
 			content = p.postProcess(a, content)
 			p.writeFile(
-				a.Name,
+				p.resolveOutputPath(a.Name),
 				[]byte(content),
 				a.Overwrite,
 				a.Perms,
@@ -185,4 +188,11 @@ func (p *stdPersister) postProcess(a Artifact, in string) string {
 	}
 
 	return string(b)
+}
+
+func (p *stdPersister) resolveOutputPath(a string) string {
+	if p.outputPath == "." {
+		return a
+	}
+	return filepath.Join(p.outputPath, a)
 }
